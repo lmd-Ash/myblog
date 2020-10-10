054e81c5261eb08b3f39593467b85df753e200e9
lmd-Ash <1192378765@qq.com>
2020-10-10 18:15:47 +0800

    备注

---
+++ src/main/java/Text.java
@@ -0,0 +1,12 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @author lmd
+ * @version 1.0.0
+ * @date 2020/10/10
+ */
+public class Text {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
