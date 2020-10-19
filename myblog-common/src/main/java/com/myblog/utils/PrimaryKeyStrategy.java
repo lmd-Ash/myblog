933f6bc881f32fcd60404f3d5b5ca5f5a1a0caef
lmd-Ash <1192378765@qq.com>
2020-10-19 14:50:16 +0800

    主键生成策略更新，使用localDate等线程安全的时间类

--- myblog-common/src/main/java/com/myblog/utils/PrimaryKeyStrategy.java
+++ myblog-common/src/main/java/com/myblog/utils/PrimaryKeyStrategy.java
@@ -2,7 +2,8 @@ package com.myblog.utils;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Random;
 
@@ -16,10 +17,46 @@ import java.util.Random;
 @Slf4j
 public class PrimaryKeyStrategy {
     /**
+     * ZoneId: 时区ID，用来确定Instant和LocalDateTime互相转换的规则
+     * Instant: 用来表示时间线上的一个点（瞬时）
+     * LocalDate: 表示没有时区的日期（年月日）, LocalDate是不可变并且线程安全的
+     * LocalTime: 表示没有时区的时间（时分秒）, LocalTime是不可变并且线程安全的
+     * LocalDateTime: 表示没有时区的日期时间, LocalDateTime是不可变并且线程安全的
+     */
+    LocalDateTime localDateTime = LocalDateTime.of(LocalDate.now(), LocalTime.now());
+    ZoneId zoneId = ZoneId.systemDefault();
+    Instant instant = localDateTime.atZone(zoneId).toInstant();
+    Date date = Date.from(instant);
+
+    /**
+     * 获取当前时间加{num}天的时间
+     * num可为负数
+     */
+    static {
+        //获取LocalDateTime类，LocalDate.now()当前年月日，LocalTime.MIN获取时间的最小值
+        LocalDateTime localDateTimeMin = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
+        //往前推15天
+        localDateTimeMin = localDateTimeMin.plusDays(-16);
+        //LocalDateTime转Date
+        ZoneId zoneMin = ZoneId.systemDefault();
+        Instant instantMin = localDateTimeMin.atZone(zoneMin).toInstant();
+        //获取开始时间
+        Date startTime = Date.from(instantMin);
+    }
+
+
+    //不推荐使用，因为线程不安全
+//    SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
+    /**
      * 每天重新开始增长
      */
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
-    private static final String DATE_STRING = DATE_FORMAT.format(new Date());
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
+    /**
+     * 获取当前日期时间 年月日
+     */
+    private static final LocalDate LOCAL_DATE = LocalDate.now();
+    private static final String DATE_STRING = LOCAL_DATE.format(FORMATTER);
+    ;
 
     /**
      * 生成int类型主键
