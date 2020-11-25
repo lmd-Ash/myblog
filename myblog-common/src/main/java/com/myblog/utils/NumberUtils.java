a40e85f6ce32a5c434727dce0604eb2bb2454bc4
lmd-Ash <1192378765@qq.com>
2020-11-25 18:04:50 +0800

    工具类更新

---
+++ myblog-common/src/main/java/com/myblog/utils/NumberUtils.java
@@ -0,0 +1,52 @@
+package com.myblog.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.regex.Pattern;
+
+/**
+ * 数字判断工具类
+ *
+ * @author lmd
+ * @version 1.0.0
+ * @date 2020/11/25
+ */
+public class NumberUtils {
+    private static final Pattern PATTERN = Pattern.compile("^[-\\+]?[\\d]*$");
+    private static final String DECIMAL_POINT = ".";
+    private static final String SPLITS = "\\.";
+
+    /**
+     * 判断string是否是数字
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isInteger(String str) {
+        if (StringUtils.isBlank(str)) {
+            return false;
+        }
+        return PATTERN.matcher(str).matches();
+    }
+
+    /**
+     * 判断string是否是数字,小数
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNumber(String str) {
+        //判断是否有小数点
+        if (str.indexOf(DECIMAL_POINT) > 0) {
+            //判断是否只有一个小数点
+            if (str.indexOf(DECIMAL_POINT) == str.lastIndexOf(DECIMAL_POINT) && str.split(SPLITS).length == 2) {
+                return isInteger(str);
+            } else {
+                return false;
+            }
+        }
+        return isInteger(str);
+
+    }
+
+}
